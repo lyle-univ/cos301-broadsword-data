61f8dcad46f4031d06e9dd8d997894097e445bbc
lyle-univ <u29562695@tuks.co.za>
2017-03-20 15:47:39 +0200

    location generator latitude generator

--- src/location_generator.py
+++ src/location_generator.py
@@ -9,3 +9,11 @@ def gen_random_longitude():
   subsec = randint(0,9)
   return str(deg)+str(min)+str(sec)+str(subsec)
 
+def gen_random_latitude():
+  deg = randint(0,90)
+  min = randint(0,60)
+  sec = randint(0,60)
+  subsec = randint(0,9)
+  return str(deg)+str(min)+str(sec)+str(subsec)
+
+
