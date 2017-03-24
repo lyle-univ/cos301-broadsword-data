cf43e4985cff19a7931a6252fffc65302a4ee29b
lyle-univ <u29562695@tuks.co.za>
2017-03-24 10:24:01 +0200

    Scripts for integration team some corrections

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 sudo apt-get install openjdk-7-jre
-git clone https://github.com/lyle-univ/cos301-broadsword-data.git
 wget http://apache.saix.net/flink/flink-1.2.0/flink-1.2.0-bin-hadoop2-scala_2.10.tgz
 tar xzf flink-*.tgz
