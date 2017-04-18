b586a5685dccfdff71bac4aa55a5ac0d9914a945
lyle-univ <u29562695@tuks.co.za>
2017-04-18 20:36:53 +0200

    Tab space issues fixed

--- src/floor_lookup.py
+++ src/floor_lookup.py
@@ -1,19 +1,19 @@
 import json
 
 class FloorLookup:
-     def __init__(self, hostname, port, username, password):
-         self.aruba_handle = aruba_wrapper.Aruba(hostname,port,username,password)
+	def __init__(self, hostname, port, username, password):
+		self.aruba_handle = aruba_wrapper.Aruba(hostname,port,username,password)
 
-    def get_json(self):
-        raw_json = aruba_handle.get("/api/v1/floor")
-        return json.loads(raw_json)
+	def get_json(self):
+		raw_json = aruba_handle.get("/api/v1/floor")
+		return json.loads(raw_json)
 
-    def lookup(self,building_id,floor_id):
-        obj = self.get_json()
-        for field in obj['Floor_result']:
-                if 'msg' in field:
-                    if building_id in field['msg']['building_id']:
-                            if floor_id in field['msg']['floor_id']:
-                                return field['msg']['floor_name']
+	def lookup(self,building_id,floor_id):
+		obj = self.get_json()
+		for field in obj['Floor_result']:
+			if 'msg' in field:
+				if building_id in field['msg']['building_id']:
+					if floor_id in field['msg']['floor_id']:
+						return field['msg']['floor_name']
                         
                 #return "Error:Cannot locate floor "
--- src/location_lookup.py
+++ src/location_lookup.py
@@ -1,18 +1,18 @@
 import json
 
 class LocationLookup:
-   def __init__(self, hostname, port, username, password):
-      self.aruba_handle = aruba_wrapper.Aruba(hostname,port,username,password)
+	def __init__(self, hostname, port, username, password):
+		self.aruba_handle = aruba_wrapper.Aruba(hostname,port,username,password)
  
 	def get_json(self,mac_addr):
-    raw_json = aruba_handle.get('/api/v1/location?sta_eth_mac='+mac_addr)
-    return json.loads(raw_json)
+		raw_json = aruba_handle.get('/api/v1/location?sta_eth_mac='+mac_addr)
+		return json.loads(raw_json)
 
 
 	def lookup(self,mac_addrress):
-	    obj = self.get_json(mac_addrress)
-	    for field in obj['Location_result']:
-		    	if 'msg' in field:
-		        	return ("{\"x\": "+str(field['msg']['sta_location_x'])+", \"y\": "+str(field['msg']['sta_location_y'])+", \"building_id\": \""+field['msg']['building_id']+"\", \"floor_id\": \""+field['msg']['floor_id']+"\"}")
+		obj = self.get_json(mac_addrress)
+		for field in obj['Location_result']:
+			if 'msg' in field:
+				return ("{\"x\": "+str(field['msg']['sta_location_x'])+", \"y\": "+str(field['msg']['sta_location_y'])+", \"building_id\": \""+field['msg']['building_id']+"\", \"floor_id\": \""+field['msg']['floor_id']+"\"}")
 		                
 		        	
