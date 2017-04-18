2cc150013c37d5f4752c062f35b164407028b828
lyle-univ <u29562695@tuks.co.za>
2017-04-18 19:06:14 +0200

    Adapted the aruba wrapper for authentication and query over https

--- src/aruba_wrapper.py
+++ src/aruba_wrapper.py
@@ -2,10 +2,17 @@ import urllib2
 
 class Aruba:
 
-    def __init__(self, host_name, port):
+    def __init__(self, host_name, port, username, password):
         self.host_name = host_name
         self.port = port
+        self.username
+        self.password
 
     "TODO: add exception handling"
     def get(self,query_string):
-        return urllib2.urlopen(self.host_name+":"+self.port+"/"+query_string).read()
+        p = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        p.add_password(None, self.host_name+":"+self.port+"/"+query_string, self.username, self.password)
+        handler = urllib2.HTTPBasicAuthHandler(p)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
+        return urllib2.urlopen(url).read()
