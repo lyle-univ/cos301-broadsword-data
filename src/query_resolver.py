012adef79b01b8b46d9ea6ad3492ae9289d063db
lyle-univ <u29562695@tuks.co.za>
2017-04-18 20:36:30 +0200

    Implemented command line arguments

--- src/query_resolver.py
+++ src/query_resolver.py
@@ -5,16 +5,42 @@ import building_lookup
 import floor_lookup
 import location_lookup
 import json
+import argparse
 
-address_port = 'http://127.0.0.1:4161'
-writer = nsq.Writer(['127.0.0.1:4150'])
+
+parser = argparse.ArgumentParser(description='Serving location requests on the an NSQ topic',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--nsqlookupd_hostname',
+                    help='The http hostname of the NSQ lookupd daemon', default='http://127.0.0.1', metavar='')
+parser.add_argument('--nsqlookupd_port',
+                    help='The port number of the NSQ lookupd daemon', default='4161', metavar='')
+parser.add_argument('--nsqlookupd_polling_interval',
+                    help='The amount of time in seconds between querying all of the supplied nsqlookupd instances. A random amount of time based on this value will be initially introduced in order to add jitter when multiple readers are running', default='15', metavar='')
+parser.add_argument('--nsqd_hostname',
+                    help='The http hostname of the NSQ daemon', default='http://127.0.0.1', metavar='')
+parser.add_argument('--nsqd_port',
+                    help='The http port of the NSQ daemon', default='4150', metavar='')
+parser.add_argument('--subscribe_topic',
+                    help='The nsq topic to listen for requests', default='data', metavar='')
+parser.add_argument('--nsq_channel',
+                    help='The channel within an NSQ topic to listen for requests', default='navup', metavar='')
+parser.add_argument('--aruba_hostname',
+                    help='The hostname of the aruba location engine', default='https://137.215.6.208', metavar='')
+parser.add_argument('--aruba_port',
+                    help='The port of the aruba location engine', default='', metavar='')
+parser.add_argument('--aruba_username',
+                    help='The username to use the aruba location engine', default='', metavar='')
+parser.add_argument('--aruba_password',
+                    help='The username to use the aruba location engine', default='', metavar='')
+args = parser.parse_args()
+
+writer = nsq.Writer([args.nsqd_hostname+':'+nsqd_port])
 
 def publish(src, dest, msgtype, content):
   result="{\"src\":\""+src+"\",\"dest\":\""+dest+"\",\"msgType\":\""+msgtype+"\",,\"queryType\":\"getCurrentLocation\",\"content\":\""+content+"\"} }"
   return result
   
 def Searcher(mac_string):
-  locationL= location_lookup.LocationLookup()
+  locationL= location_lookup.LocationLookup(args.aruba_hostname,args.aruba_port,args.username_port,args.password_port)
   location_json=json.loads(locationL.lookup(mac_string))
   buildingID=location_json['building_id']
   floorID=location_json['floor_id']
@@ -22,10 +48,10 @@ def Searcher(mac_string):
   y=location_json['y']
 
   
-  floorL=floor_lookup.FloorLookup()
+  floorL=floor_lookup.FloorLookup(args.aruba_hostname,args.aruba_port,args.username_port,args.password_port)
   floor_name=floorL.lookup(buildingID,floorID)
 
-  buildingL= building_lookup.BuildingLookup()
+  buildingL= building_lookup.BuildingLookup(args.aruba_hostname,args.aruba_port,args.username_port,args.password_port)
   building_name=buildingL.lookup(buildingID)
 
   final_content=("{ \"mac_address\": \""+mac_string+"\" ,\"x\": "+str(x)+", \"y\": "+str(y)+", \"building_name\": \""+building_name+"\", \"floor_name\": \""+floor_name+"\"}")
@@ -52,6 +78,6 @@ def finish_pub(conn, data):
 
 
 
-r = nsq.Reader(message_handler=handler, lookupd_http_addresses=[address_port],
-                topic='gis', channel='navup', lookupd_poll_interval=15)
+r = nsq.Reader(message_handler=handler, lookupd_http_addresses=[args.nsqlookupd_hostname+':'+args.nsqlookupd_port],
+                topic=args.subscribe_topic, channel=args.nsq_channel, lookupd_poll_interval=args.nsqlookupd_polling_interval)
 nsq.run()
