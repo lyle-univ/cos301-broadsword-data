70463bc6f397dece6951babc364eabf9e5ac842a
Seonin <seonindavid@gmail.com>
2017-04-19 13:07:06 +0200

    Update aruba_wrapper.py

--- src/aruba_wrapper.py
+++ src/aruba_wrapper.py
@@ -5,13 +5,14 @@ class Aruba:
     def __init__(self, host_name, port, username, password):
         self.host_name = host_name
         self.port = port
-        self.username
-        self.password
+        self.username=username
+        self.password=password
 
     "TODO: add exception handling"
     def get(self,query_string):
+        url=self.host_name+"/"+query_string
         p = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        p.add_password(None, self.host_name+":"+self.port+"/"+query_string, self.username, self.password)
+        p.add_password(None, url, self.username, self.password)
         handler = urllib2.HTTPBasicAuthHandler(p)
         opener = urllib2.build_opener(handler)
         urllib2.install_opener(opener)
