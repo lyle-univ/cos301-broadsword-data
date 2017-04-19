e99be055014a08862a2afbbff38cca692d8350f3
Keegan Ferrett <keegan.ferrett@gmail.com>
2017-04-19 23:00:11 +0200

    Updated regex to include all modules

--- src/malformedTest.py
+++ src/malformedTest.py
@@ -7,7 +7,7 @@ class validateRequest(object):
 	"""class for validating request"""
 	def __init__(self):
 		print "Hello"
-		self.pattern = re.compile("^{\"src\":\"(gis|navigation)\",\"dest\":\"(gis|navigation)\",\"msgType\":\"request\",\"queryType\":\"getCurrentLocation\",\"content\":{\"mac\":\"[0-9 a-f]{1,2}:[0-9 a-f]{1,2}:[0-9 a-f]{1,2}:[0-9 a-f]{1,2}:[0-9 a-f]{1,2}:[0-9 a-f]{1,2}\"}}$")
+		self.pattern = re.compile("""^{\"src\":\"(gis|navigation|data|access|users|notifications)\",\"dest\":\"(gis|navigation|data|access|users|notifications)\",\"msgType\":\"request\",\"queryType\":\"getCurrentLocation\",\"content\":{\"mac\":\"[0-9 A-F a-f]{1,2}:[0-9 A-F a-f]{1,2}:[0-9 A-F a-f]{1,2}:[0-9 A-F a-f]{1,2}:[0-9 A-F a-f]{1,2}:[0-9 A-F a-f]{1,2}\"}}$""")
 	
 	"""Created class for this function for speed. regular expressions are slow to compile 
 		but now it is only compiled once and not every fucntion call"""
@@ -17,6 +17,7 @@ class validateRequest(object):
 		result = self.pattern.match(request)
 
 		if result:
+			print "passed"
 			return True
 		else:
 			errorString = "[%s] [Malformed Data] [%s]" % (str(datetime.now() ), request)
@@ -24,7 +25,7 @@ class validateRequest(object):
 			logging.error(errorString)
 			return False	
 
-x = validateRequest()
-while True:
+#x = validateRequest()
+#while True:
 	#Currently invalid request due to gi5s
-	x.validate("{\"src\"  : \"gi5s\",\"dest\" : \"navigation\",\"msgType\" : \"request\",\"queryType\":\"getCurrentLocation\", \"content\" : {\"mac\" : \"bf:f0:c4:ab:1f:e2\"} }")
+	#x.validate("{\"src\"  : \"gis\",\"dest\" : \"gis\",\"msgType\" : \"request\",\"queryType\":\"getCurrentLocation\", \"content\" : {\"mac\" : \"bf:f0:c4:AB:1f:e2\"} }")
