b52385beaa2d1cbe531cf0790962f06c7e5fee6e
Seonin <seonindavid@gmail.com>
2017-04-21 09:29:07 +0200

    Update location_lookup.py

--- src/location_lookup.py
+++ src/location_lookup.py
@@ -6,7 +6,7 @@ class LocationLookup:
 		self.aruba_handle = aruba_wrapper.Aruba(hostname,port,username,password)
  
 	def get_json(self,mac_addr):
-		raw_json = self.aruba_handle.get('/api/v1/location?sta_eth_mac='+mac_addr)
+		raw_json = self.aruba_handle.get('mock_location_json')
 		return json.loads(raw_json)
 
 
@@ -16,4 +16,18 @@ class LocationLookup:
 			if 'msg' in field:
 				return ("{\"x\": "+str(field['msg']['sta_location_x'])+", \"y\": "+str(field['msg']['sta_location_y'])+", \"building_id\": \""+field['msg']['building_id']+"\", \"floor_id\": \""+field['msg']['floor_id']+"\"}")
 		                
-		        	
+
+class TestLocationLookup:
+	def __init__(self, hostname, port, username, password):
+		self.loc = LocationLookup("127.0.0.1","80","","")
+ 
+	def get_mock_json(self,mac_addr):
+		raw_json = open('mock_location_json', 'r').read()
+		return json.loads(raw_json)
+
+	def test_lookup(self):
+		self.loc.get_json = self.get_mock_json
+		if(self.loc.lookup("58:48:22:a7:84:6b")=="{\"x\": "+str(55.566734)+", \"y\": "+str(42.82108)+", \"building_id\": \"08BEB4DE95EA3D6B999119FAA9B0F1C1\", \"floor_id\": \"0D80FE42238A3B97BB84574CF1C6B2F3\"}"):
+			print "simple mock location test passed"
+		else:
+			print "simple mock location test failed"
